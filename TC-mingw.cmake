3a362e495eb2fdefe3e9cce14ac49aff35f4a196
Navaneeth Rao <32010211+navaneeth-dev@users.noreply.github.com>
2024-06-28 12:07:41 +0530

    fix: msvc compile and cross compile

--- TC-mingw.cmake
+++ TC-mingw.cmake
@@ -1,9 +1,5 @@
 set(CMAKE_SYSTEM_NAME Windows)
 
 # set compiler settings
-set( CMAKE_CXX_STANDARD 20 )
 set( CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++ )
-set( CMAKE_CXX_FLAGS "-Wl,--pic-executable,-e,main -Wl,-Bstatic -s -w -ffunction-sections -fdata-sections -Wno-write-strings -municode -fno-exceptions -fmerge-all-constants -static-libgcc -Wl,-Bstatic" )
-
-# linking library
-link_libraries( netapi32 ws2_32 wsock32 wtsapi32 iphlpapi )
+set( CMAKE_CXX_FLAGS "-municode -fno-exceptions -static-libgcc -static" )
